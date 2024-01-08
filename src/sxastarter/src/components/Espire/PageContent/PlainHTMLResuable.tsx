e865e13c2a1277f4bd764c87ac4cb755c02a3fab
SitecoreAdmin <SitecoreAdmin@RSMDocker-VM>
2024-01-08 12:26:45 +0000

    fix PlainHTMLResuable Component

--- src/sxastarter/src/components/Espire/PageContent/PlainHTMLResuable.tsx
+++ src/sxastarter/src/components/Espire/PageContent/PlainHTMLResuable.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { PlainHTMLResuableTemplateProps } from 'lib/component-props/EspireTemplateProps/PageContent/PlainHTMLResuableTemplateProps';
 import { withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
 
-export const PlainHTMLResuable = (props: PlainHTMLResuableTemplateProps): JSX.Element => {
+export const PlainHTMLReusable = (props: PlainHTMLResuableTemplateProps): JSX.Element => {
   return (
     <div className={`plain-text-reusable ${props.params.styles}`}>
       <div dangerouslySetInnerHTML={{ __html: props?.fields?.PlainHTML?.value }} />
     </div>
   );
 };
-export default withDatasourceCheck()<PlainHTMLResuableTemplateProps>(PlainHTMLResuable);
+export default withDatasourceCheck()<PlainHTMLResuableTemplateProps>(PlainHTMLReusable);
