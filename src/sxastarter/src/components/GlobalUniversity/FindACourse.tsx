24a1011722f44850ed895cab671e37f93c9c1806
SitecoreAdmin <SitecoreAdmin@RSMDocker-VM>
2024-01-23 06:06:55 +0000

    Added Title for find a course

--- src/sxastarter/src/components/GlobalUniversity/FindACourse.tsx
+++ src/sxastarter/src/components/GlobalUniversity/FindACourse.tsx
@@ -22,37 +22,40 @@ const FindACourse = (props: FindACourseProps): JSX.Element => {
   console.log(props, 'props');
   return (
     <div className={`${props.params.styles}`}>
-      <div className="language-selector">
-        <form className="row g-3">
-          <div className=" col-auto ">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Search by name..."
-              aria-label="Sizing example input"
-              aria-describedby="inputGroup-sizing-default"
-            />
-          </div>
-          <div className=" col-auto">
-            <select className="form-select" aria-label="Default select example">
-              <option selected>Search by category</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
-          <div className=" col-auto">
-            <select className="form-select" aria-label="Default select example">
-              <option selected>Select type</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
-          <button type="submit" className="btn btn-primary col-auto">
-            Submit
-          </button>
-        </form>
+      <div className="container">
+        <div id="find-a course">
+          <h4>Find a course </h4>
+          <form className="row g-3">
+            <div className=" col-auto ">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search by name..."
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-default"
+              />
+            </div>
+            <div className=" col-auto">
+              <select className="form-select" aria-label="Default select example">
+                <option selected>Search by category</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <div className=" col-auto">
+              <select className="form-select" aria-label="Default select example">
+                <option selected>Select type</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <button type="submit" className="btn btn-primary col-auto">
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
