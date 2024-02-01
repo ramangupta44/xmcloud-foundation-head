00e0f1544d6ef72878739a54596b8d1d80049d96
SitecoreAdmin <SitecoreAdmin@RSMDocker-VM>
2024-02-01 06:04:09 +0000

    Find A Course Lisitng

--- src/sxastarter/src/components/GlobalUniversity/FindACourse.tsx
+++ src/sxastarter/src/components/GlobalUniversity/FindACourse.tsx
@@ -1,15 +1,20 @@
-import { Field } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Field, LinkField, Link as JssLink } from '@sitecore-jss/sitecore-jss-nextjs';
+import { useSearchParams } from 'next/navigation';
 import { ComponentProps } from 'lib/component-props';
-// import Form from 'react-bootstrap/Form';
+import Link from 'next/link';
+import { useState } from 'react';
+import { EditMode } from 'lib/component-props';
 
 type FindACourseProps = ComponentProps & {
   params: { [key: string]: string };
   fields: {
     Title: Field<string>;
-    Category: Category[];
+    CourseCategory: CourseCategory[];
+    Link: LinkField;
+    CourseType: CourseType[];
   };
 };
-type Category = {
+type CourseCategory = {
   fields: {
     [key: string]: {
       [key: string]: {
@@ -18,10 +23,19 @@ type Category = {
     };
   };
 };
+type CourseType = CourseCategory;
+
 const FindACourse = (props: FindACourseProps): JSX.Element => {
-  console.log(props, 'props');
+  const [category, setCategory] = useState(' ');
+  const [type, setType] = useState(' ');
+  const [text, setText] = useState('');
+  const searchParams = useSearchParams();
+  const isEdit = EditMode();
+  const linkPath = props?.fields?.Link?.value?.href;
+  const linkText = props?.fields?.Link?.value?.text;
+
   return (
-    <div className={`${props.params.styles}`} id="find-a-course">
+    <div className={`${props?.params?.styles}`} id="find-a-course">
       <div className="container">
         <div className="find-a-course">
           <h4>Find a course </h4>
@@ -30,35 +44,72 @@ const FindACourse = (props: FindACourseProps): JSX.Element => {
               <input
                 type="text"
                 className="form-control"
+                value={text}
                 placeholder="Search by name..."
                 aria-label="Sizing example input"
                 aria-describedby="inputGroup-sizing-default"
+                onChange={(e) => setText(e.target.value)}
               />
             </div>
             <div className=" col-md-3 col-xs-12">
-              <select className="form-select" aria-label="Default select example">
-                <option selected>Search by category</option>
-                <option value="1">Program</option>
-                <option value="2">Accounting</option>
-                <option value="3">Administration & Business</option>
-                <option value="4">Aged Care and Disability</option>
-                <option value="5">Agriculture</option>
+              <select
+                className="form-select"
+                aria-label="Default select example"
+                name={'category'}
+                defaultValue={searchParams.get('category') as string}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value=" ">Search By Category</option>
+                {props?.fields?.CourseCategory?.map((item, index) => {
+                  return (
+                    <option
+                      key={index}
+                      value={(item?.fields?.Title?.value as unknown as string).toLocaleLowerCase()}
+                    >
+                      {item?.fields?.Title?.value as unknown as string}
+                    </option>
+                  );
+                })}
               </select>
             </div>
             <div className=" col-md-3 col-xs-12">
-              <select className="form-select" aria-label="Default select example">
-                <option selected>Select type</option>
-                <option value="1">Accredited Courses</option>
-                <option value="2">Courses</option>
-                <option value="3">Short Courses</option>
-                <option value="4">VETDSS courses</option>
-                <option value="5">Apprentice/Traineeships</option>
+              <select
+                className="form-select"
+                aria-label="Default select example"
+                defaultValue={searchParams.get('type') as string}
+                onChange={(e) => setType(e.target.value)}
+              >
+                <option value={' '}>Select Type</option>
+                {props?.fields?.CourseType?.map((item, index) => {
+                  return (
+                    <option
+                      key={index}
+                      value={(item?.fields?.Title?.value as unknown as string).toLocaleLowerCase()}
+                    >
+                      {item?.fields?.Title?.value as unknown as string}
+                    </option>
+                  );
+                })}
               </select>
             </div>
             <div className=" col-md-3 col-xs-12">
-              <button type="submit" className="btn btn-primary col-auto">
-                Submit
-              </button>
+              {isEdit ? (
+                <JssLink field={props?.fields?.Link} className="btn btn-primary col-auto" />
+              ) : (
+                <Link
+                  className="btn btn-primary col-auto"
+                  href={{
+                    pathname: `${linkPath?.toLocaleLowerCase()}`,
+                    query: {
+                      keyword: text,
+                      category: category.toLocaleLowerCase(),
+                      type: type.toLocaleLowerCase(),
+                    },
+                  }}
+                >
+                  {linkText}
+                </Link>
+              )}
             </div>
           </form>
         </div>
