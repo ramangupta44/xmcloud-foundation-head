5e81709fd895d4d99efa437b6a1f7522e726925d
SitecoreAdmin <SitecoreAdmin@RSMDocker-VM>
2024-02-09 08:24:02 +0000

    Yml changes for BlogDetails

--- src/sxastarter/src/components/GlobalUniversity/CourseInfoBadge.tsx
+++ src/sxastarter/src/components/GlobalUniversity/CourseInfoBadge.tsx
@@ -60,3 +60,36 @@ export const Default = (props: CourseInfoBadgeProps): JSX.Element => {
     </ComponentCourseInfoBadge>
   );
 };
+export const Badge = (props: CourseInfoBadgeProps): JSX.Element => {
+  const { sitecoreContext } = useSitecoreContext();
+  const id = props?.params?.RenderingIdentifier;
+
+  if (!(props?.fields && props?.fields?.Title) && !sitecoreContext?.route?.fields?.Title) {
+    return (
+      <div
+        className={`component courseinfobadge ${props?.params?.styles}`}
+        id={id ? id : undefined}
+      >
+        <div className="component-courseinfobadge badge">
+          <div className="field-courseinfobadge badge">[CourseDescription]</div>
+        </div>
+      </div>
+    );
+  }
+
+  const field = (
+    props?.fields && props?.fields?.Title
+      ? props?.fields?.Title
+      : sitecoreContext?.route?.fields?.Title
+  ) as Field<string>;
+
+  return (
+    <ComponentCourseInfoBadge styles={props?.params?.styles} id={id}>
+      <div className={`component courseinfobadge badge ${props.params.styles}`} id={id ? id : undefined}>
+        <div className="component-courseinfobadge badge">
+          <Text field={field} tag="h6" className="field-title" />
+        </div>
+      </div>
+    </ComponentCourseInfoBadge>
+  );
+};
