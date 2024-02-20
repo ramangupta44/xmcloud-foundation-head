3e813c98cff5e26327ef1e529e671d3cd720a369
SitecoreAdmin <SitecoreAdmin@RSMDocker-VM>
2024-02-20 11:08:13 +0000

    fix build issue

--- src/sxastarter/src/Layout.tsx
+++ src/sxastarter/src/Layout.tsx
@@ -8,7 +8,7 @@ import config from 'temp/config';
 import Scripts from 'src/Scripts';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
-import Cookie from 'components/Espire/Header/Cookie';
+// import Cookie from 'components/Espire/Header/Cookie';
 // Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
 // If you're not supporting the Experience Editor, you can remove this.
 const publicUrl = config.publicUrl;
