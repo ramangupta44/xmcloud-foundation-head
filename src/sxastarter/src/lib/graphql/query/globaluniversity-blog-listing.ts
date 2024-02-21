cfee1b8635b1b0b55a06f1456705b16b4e2899fc
SitecoreAdmin <SitecoreAdmin@RSMDocker-VM>
2024-02-21 10:49:08 +0000

    BlogListing Variant Changes

---
+++ src/sxastarter/src/lib/graphql/query/globaluniversity-blog-listing.ts
@@ -0,0 +1,42 @@
+import { gql } from '@apollo/client';
+export const GlobalUniversity_Blog_Listing_Query = gql`
+  query BlogListing(
+    $path: String
+    $template: String    
+  ) {
+    BlogListingData: search(
+      where: {
+        AND: [
+          { name: "_path", value: $path, operator: CONTAINS }
+          { name: "_templates", value: $template, operator: EQ }
+        
+        ]
+      }
+      first: 10
+      
+    ) {
+      total
+      pageInfo {
+        endCursor
+        hasNext
+      }
+      results {
+        ... on BlogDetails {
+          title {
+            value
+          }
+          thumbnailImage {
+            value
+            jsonValue
+          }
+          content {
+            value
+          }
+        }
+        url {
+          path
+        }
+      }
+    }
+  }
+`;
