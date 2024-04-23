0924352e0d002a35afec961533f4c4b9926e1802
SitecoreAdmin <SitecoreAdmin@RSMDocker-VM>
2024-04-23 11:30:41 +0000

    Security Headers Implementation

---
+++ src/sxastarter/src/security-headers/security-headers.js
@@ -0,0 +1,25 @@
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'permissions-policy',
+    value: 'accelerometer=(), camera=(), microphone=(), usb=(), interest-cohort=()',
+  },
+];
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
